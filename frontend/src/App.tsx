5a2152b7bf5ccafe489019c514d89c6043d427e0
Your Name <you@example.com>
2026-06-24 06:35:29 +0700

    tailwind integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>Hello, Vite!</h1>
+    <div className="flex items-center justify-center min-h-screen bg-slate-900 text-white">
+      <h1 className="text-4xl font-extrabold text-sky-400 drop-shadow-md">
+        Hello, Vite + Tailwind CSS!
+      </h1>
     </div>
   )
 }
